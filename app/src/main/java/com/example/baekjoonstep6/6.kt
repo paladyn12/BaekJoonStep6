47a682fa7c0c2094126d55aef190d8b5aa1c4624
Taewan Kim <paladyn12@naver.com>
2022-08-12 15:43:49 +0900

    오답 수정

--- app/src/main/java/com/example/baekjoonstep6/6.kt
+++ app/src/main/java/com/example/baekjoonstep6/6.kt
@@ -3,7 +3,5 @@ package com.example.baekjoonstep6
 import java.util.*
 
 fun main(Args:Array<String>) = with(Scanner(System.`in`)){
-    var str = nextLine()
-    var strArray = str.split(' ')
-    print(strArray.size)
+    print(StringTokenizer(nextLine()," ").countTokens())
 }
--- app/src/main/java/com/example/baekjoonstep6/8.kt
+++ app/src/main/java/com/example/baekjoonstep6/8.kt
@@ -1,23 +1,24 @@
 package com.example.baekjoonstep6
 
-import java.util.*
+import java.io.*
 
-fun main(Args:Array<String>) = with(Scanner(System.`in`)){
-    var str = next()
+fun main(){
+    var br = BufferedReader(InputStreamReader(System.`in`))
+    var str = br.readLine()
     var sum = 0
     for(i in 0..str.length-1){
-        when(str.get(i)){
+        when(str[i]){
             'A','B','C' -> sum+= 3
             'D','E','F' -> sum+= 4
             'G','H','I' -> sum+= 5
             'J','K','L' -> sum+= 6
             'M','N','O' -> sum+= 7
-            'P','Q','S','S' -> sum+= 8
+            'P','Q','R','S' -> sum+= 8
             'T','U','V' -> sum+= 9
             'W','X','Y','Z' -> sum+= 10
-            else -> sum+= 11
         }
     }
     print(sum)
+    br.close()
 }
 // Buffer 사용하여 메모리 단축
